e4831b0d038a9d07736b34e591812812857ca698
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-25 16:54:40 +0200

    feat: update form post page

--- packages/web/src/pages/FormPost/FormPost.js
+++ packages/web/src/pages/FormPost/FormPost.js
@@ -1,11 +1,43 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import Button from "../../components/Button";
 import PostForm from "../../components/PostForm";
 import MainLayout from "../../layout/MainLayout";
+import { authSelector } from "../../redux/auth/authSelector";
+import * as ROUTES from "../../routes";
+import { Card, CardBody, CardTitle, Container, LinkWrap } from "./styles";
 
 const FormPost = () => {
+  const { isAuthenticated } = useSelector(authSelector);
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(ROUTES.SIGN_IN);
+  };
+
   return (
     <MainLayout>
-      <PostForm />
+      {isAuthenticated ? (
+        <Container>
+          <Card>
+            <PostForm />
+          </Card>
+        </Container>
+      ) : (
+        <Card>
+          <CardTitle>Sign up to continue</CardTitle>
+          <CardBody>
+            You&apos;ll need to sign in before upload any content
+          </CardBody>
+          <Button primary onClick={handleClick}>
+            Sign In
+          </Button>
+          <LinkWrap>
+            <Link to={ROUTES.SIGN_UP}>Don&apos;t have an account? Sign Up</Link>
+          </LinkWrap>
+        </Card>
+      )}
     </MainLayout>
   );
 };
---
+++ packages/web/src/pages/FormPost/styles.js
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { COLOR } from "../../styles/colors";
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 32px;
+  width: 100%;
+  height: 100%;
+  margin: 24px auto;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+  @media screen and (max-width: 480px) {
+    width: 100%;
+    padding: 24px 0;
+  }
+`;
+
+export const CardTitle = styled.h1`
+  color: ${COLOR.deepPurple500};
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 16px;
+  @media screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`;
+
+export const CardBody = styled.p`
+  font-size: 16px;
+  margin-bottom: 24px;
+`;
+
+export const LinkWrap = styled.div`
+  text-align: right;
+  margin-top: 16px;
+  & * {
+    text-decoration: none;
+    font-size: 14px;
+    color: ${COLOR.textSecondary};
+  }
+  @media screen and (max-width: 480px) {
+    text-align: center;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 24px;
+`;
+
+export const NavbarLink = styled(Link)`
+  margin-right: 16px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLOR.deepPurple500};
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+  &:hover {
+    color: ${COLOR.deepPurple400};
+    transition: all 300ms ease-in-out;
+  }
+  &.active {
+    border-bottom: 3px solid ${COLOR.deepPurple400};
+  }
+`;
