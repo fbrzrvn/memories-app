c65b3ab46f39d69752fc580be39b68954ef0dd55
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-26 16:37:39 +0200

    feat: upgrade post card and material ui icons

--- packages/web/src/components/PostCard/PostCard.js
+++ packages/web/src/components/PostCard/PostCard.js
@@ -1,28 +1,57 @@
+import { Button } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import moment from "moment";
 import { object } from "prop-types";
 import React from "react";
+import { useSelector } from "react-redux";
+import { authSelector } from "../../redux/auth/authSelector";
 import {
   Card,
   PostFooter,
   PostH2,
   PostImg,
   PostInfo,
+  PostOverlay,
+  PostOverlay2,
   PostP,
   PostSpan,
 } from "./styles";
 
 const PostCard = ({ post }) => {
+  const { currentUser } = useSelector(authSelector);
+
   return (
     <Card>
-      {post.category !== "Joke" && (
-        <PostImg src={post.media || post.url} alt={post.title} />
-      )}
+      <PostImg src={post.media} alt={post.title} />
+      <PostOverlay>
+        <PostSpan>{post.name}</PostSpan>
+        <PostSpan>{moment(post.createdAt).fromNow()}</PostSpan>
+      </PostOverlay>
+      <PostOverlay2>
+        {(currentUser?.user?.googleId === post?.author ||
+          currentUser?.user?._id === post?.author) && (
+          <Button style={{ color: "#F5F6F7" }} size="small">
+            <MoreVertIcon />
+          </Button>
+        )}
+      </PostOverlay2>
       <PostInfo>
+        <PostP>{post.tags.map((tag) => `#${tag} `)}</PostP>
         <PostH2>{post.title}</PostH2>
-        {post.category === "Joke" && <PostP joke>{post.description}</PostP>}
         <PostFooter>
-          <PostSpan>{post.author}</PostSpan>
-          <PostSpan>{moment(post.createdAt).fromNow()}</PostSpan>
+          <Button size="small" color="primary">
+            <ThumbUpAltOutlined fontSize="small" />
+            &nbsp; Like
+          </Button>
+          {(currentUser?.user?.googleId === post?.author ||
+            currentUser?.user?._id === post?.author) && (
+            <Button size="small" color="primary">
+              <DeleteIcon fontSize="small" />
+              &nbsp; Delete
+            </Button>
+          )}
         </PostFooter>
       </PostInfo>
     </Card>
--- packages/web/src/components/PostCard/styles.js
+++ packages/web/src/components/PostCard/styles.js
@@ -6,18 +6,44 @@ export const Card = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  padding-bottom: 24px;
-  margin-bottom: 16px;
+  padding-bottom: 8px;
+  margin: 16px;
   border-radius: 10px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+  position: relative;
   cursor: pointer;
   transition: all 0.2 ease-in-out;
 `;
 
 export const PostImg = styled.img`
   width: 250px;
+  height: 200px;
   aspect-ratio: 16/9;
   border-radius: 10px 10px 0 0;
+  background: rgba(0, 0, 0, 0.9);
+`;
+
+export const PostOverlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 16px;
+  left: 16px;
+`;
+
+export const PostSpan = styled.span`
+  color: ${COLOR.background};
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 4px;
+`;
+
+export const PostOverlay2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 16px;
+  right: 0;
 `;
 
 export const PostInfo = styled.div`
@@ -25,29 +51,23 @@ export const PostInfo = styled.div`
   display: flex;
   flex-direction: column;
   width: 250px;
-  padding: 0 16px;
-`;
-
-export const PostH2 = styled.h2`
-  color: ${COLOR.deepPurple500};
-  font-size: 24px;
-  margin: 16px 0 8px;
+  padding: 8px 16px 0;
 `;
 
 export const PostP = styled.p`
   color: ${COLOR.text};
-  font-size: 24px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+export const PostH2 = styled.h2`
+  font-size: 32px;
   font-weight: 400;
-  margin: 0 0 10px;
+  margin: 8px 0 16px;
 `;
 
 export const PostFooter = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-export const PostSpan = styled.span`
-  color: ${COLOR.textSecondary};
-  font-size: 12px;
 `;
