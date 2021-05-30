723fb629aa1388a550ca82dd35b78b7004e7685b
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-30 13:25:41 +0200

    feat: add client url

--- packages/web/src/api/index.js
+++ packages/web/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("USER_PROFILE")) {
