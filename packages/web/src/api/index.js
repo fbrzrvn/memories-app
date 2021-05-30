80e1ddade14c34983a3c5f0d498cd6557f034301
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-30 13:32:46 +0200

    fix: client url

--- packages/web/src/api/index.js
+++ packages/web/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:4000",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("USER_PROFILE")) {
