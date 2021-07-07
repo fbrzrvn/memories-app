d5b2da2a070d98e7ac83d724b1649607fc52df1c
Fabrizio Ervini <fabri.es018@gmail.com>
2021-07-07 13:58:31 +0200

    feat: add parser, dompurify and IconBtn

--- packages/web/src/components/PostDetails/PostDetails.js
+++ packages/web/src/components/PostDetails/PostDetails.js
@@ -1,8 +1,20 @@
+import {
+  Delete as DeleteIcon,
+  MoreVert as MoreVertIcon,
+} from "@material-ui/icons";
+import { sanitize } from "dompurify";
+import parse from "html-react-parser";
 import { object } from "prop-types";
 import React from "react";
+import { useSelector } from "react-redux";
 import { countReadingTime, formatPostDate } from "../../helper";
+import { authSelector } from "../../redux/auth/authSelector";
+import { DELETE, LIKE, UPDATE } from "../../utils/constant";
+import BtnIcon from "../IconBtn";
+import LikePost from "../PostsCard/LikePost";
 import {
   AuthorAvatar,
+  PostActions,
   PostAuthor,
   PostAuthorName,
   PostContent,
@@ -18,7 +30,11 @@ import {
 } from "./styles";
 
 const PostDetails = ({ post }) => {
+  const { currentUser } = useSelector(authSelector);
   const readingTime = countReadingTime(post.title, post.description);
+  const cleanHTML = sanitize(post.description, {
+    USE_PROFILES: { html: true },
+  });
 
   return (
     <PostWrapper>
@@ -32,6 +48,30 @@ const PostDetails = ({ post }) => {
           <PostSmall>{formatPostDate(post.createdAt)}</PostSmall>
           <PostSmall dot>·</PostSmall>
           <PostSmall>{readingTime} min read</PostSmall>
+          <PostActions>
+            <BtnIcon
+              action={LIKE}
+              post={post}
+              currentUser={currentUser}
+              icon={<LikePost post={post} currentUser={currentUser.user} />}
+            />
+            {currentUser?.user?._id === post?.author?._id && (
+              <>
+                <BtnIcon
+                  action={UPDATE}
+                  post={post}
+                  currentUser={currentUser}
+                  icon={<MoreVertIcon />}
+                />
+                <BtnIcon
+                  action={DELETE}
+                  post={post}
+                  currentUser={currentUser}
+                  icon={<DeleteIcon />}
+                />
+              </>
+            )}
+          </PostActions>
         </PostInfo>
       </PostHeader>
       <PostHero>
@@ -39,7 +79,7 @@ const PostDetails = ({ post }) => {
       </PostHero>
       <PostContent>
         <PostTags>{post.tags.map((tag) => `#${tag} `)}</PostTags>
-        <PostP>{post.description}</PostP>
+        <PostP>{parse(cleanHTML)}</PostP>
       </PostContent>
     </PostWrapper>
   );
