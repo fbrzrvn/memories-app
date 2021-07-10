f28c9f127de64678016e127c741ce9ba64b03e1d
Fabrizio Ervini <fabri.es018@gmail.com>
2021-07-10 11:21:34 +0200

    feat: add user page

--- packages/web/src/pages/User/User.js
+++ packages/web/src/pages/User/User.js
@@ -1,47 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PostCard from "../../components/PostsCard";
+import UserHero from "../../components/UserHero";
 import MainLayout from "../../layout/MainLayout";
 import { authSelector } from "../../redux/auth/authSelector";
-import {
-  AuthorAvatar,
-  AuthorHeaders,
-  AuthorHero,
-  AuthorInfo,
-  AuthorStats,
-  AuthorSubtitle,
-  AuthorTitle,
-} from "./styles";
+import { fetchUserPosts } from "../../redux/user/userActions";
+import { userSelector } from "../../redux/user/userSelector";
+import { PostsWrapper } from "./styles";
 
 const User = () => {
+  const dispatch = useDispatch();
   const {
     currentUser: { user },
   } = useSelector(authSelector);
+  const { posts } = useSelector(userSelector);
+
+  useEffect(() => {
+    dispatch(fetchUserPosts());
+  }, [dispatch]);
 
   return (
     <MainLayout>
-      <AuthorHero>
-        <AuthorInfo>
-          <AuthorAvatar src={user?.imageUrl} alt={user?.name} />
-          <AuthorHeaders>
-            <AuthorTitle>{user?.name}</AuthorTitle>
-            <AuthorSubtitle>{user?.email}</AuthorSubtitle>
-          </AuthorHeaders>
-        </AuthorInfo>
-        <AuthorStats>
-          <div>
-            <p>5</p>
-            <p>posts</p>
-          </div>
-          <div>
-            <p>5</p>
-            <p>followers</p>
-          </div>
-          <div>
-            <p>5</p>
-            <p>following</p>
-          </div>
-        </AuthorStats>
-      </AuthorHero>
+      <UserHero user={user} userPosts={posts} />
+      <PostsWrapper>
+        {posts?.map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </PostsWrapper>
     </MainLayout>
   );
 };
--- packages/web/src/pages/User/styles.js
+++ packages/web/src/pages/User/styles.js
@@ -1,65 +1,13 @@
-import { Avatar } from "@material-ui/core";
 import styled from "styled-components";
 
-export const AuthorHero = styled.div`
-  display: flex;
-  padding: 12px 0;
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
-
-export const AuthorInfo = styled.div`
+export const PostsWrapper = styled.div`
   display: grid;
-  grid-template-columns: auto 1fr;
+  grid-template-columns: repeat(auto-fit, 300px);
+  grid-gap: 16px;
+  justify-content: center;
   align-items: center;
-  width: 100%;
-  @media screen and (max-width: 768px) {
-    margin-bottom: 32px;
-  }
-`;
-
-export const AuthorAvatar = styled(Avatar)`
-  && {
-    height: 72px;
-    width: 72px;
-    @media screen and (max-width: 375px) {
-      height: 44px;
-      width: 44px;
-    }
-  }
-`;
-
-export const AuthorHeaders = styled.div`
-  margin-left: 12px;
-`;
-
-export const AuthorTitle = styled.h2`
-  font-size: 32px;
-  line-height: 1;
-  margin: 0;
+  margin-top: 50px;
   @media screen and (max-width: 375px) {
-    font-size: 22px;
+    grid-template-columns: repeat(auto-fit, 230px);
   }
-`;
-
-export const AuthorSubtitle = styled.h5`
-  font-size: 20px;
-  margin: 0;
-  @media screen and (max-width: 375px) {
-    font-size: 18px;
-  }
-`;
-
-export const AuthorStats = styled.div`
-  min-width: 300px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-export const AuthorSmall = styled.small`
-  font-size: 16px;
-  line-height: 18px;
-  margin: 0;
 `;
